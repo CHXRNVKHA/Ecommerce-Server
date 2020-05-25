b2750014b15739b1bec6a6cabe1d50413af37d05
Dmitry Veko <vekodmitriy0@gmail.com>
2020-05-25 15:43:03 +0300

    feat: add cors config to server

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 const app = express();
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: 'Content-type, Authorization, Origin, X-Requested-With, Accept',
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
