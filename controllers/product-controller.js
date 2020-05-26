787b1fba6b4809b4c0c3dfd65ae585c025a34f63
Dmitry Veko <vekodmitriy0@gmail.com>
2020-05-26 13:06:04 +0300

    feat: add getAll function for product services

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -4,6 +4,15 @@ class ProductController {
     getAllProducts = async (req, res) => {
         try {
             const result = await service.getAll(req);
+            if (result.length > 0) {
+                res.status(200).json({
+                    count: result.length,
+                    products: result,
+                });
+            }
+            else {
+                res.json({message: 'No products founds'});
+            }
             res.send(result);
         }
         catch (e) {
--- services/product-service.js
+++ services/product-service.js
@@ -1,35 +1,42 @@
 const {database} = require('../config/helpers');
-let page = (req.query.page != undefined && req.query.page != 0) ? req.query.page : 1;
-let limit = (req.query.limit != undefined && req.query.limit != 0) ? req.query.limit : 10;
 
-let startVal;
-let endVal;
+const getAll = function (req) {
+    let page = (req.query.page != undefined && req.query.page != 0) ? req.query.page : 1;
+    let limit = (req.query.limit != undefined && req.query.limit != 0) ? req.query.limit : 10;
 
-if (page > 0) {
-    startVal = (page * limit) - limit;
-    endVal = page * limit;
+    let startVal;
+    let endVal;
+
+    if (page > 0) {
+        startVal = (page * limit) - limit;
+        endVal = page * limit;
+    }
+    else {
+        startVal = 0; 
+        endVal = 10;
+    }
+
+    database.table('products as p')
+        .join([{
+            table: 'categories as c',
+            on: 'c.id = p.cat_id',
+        }])
+        .withFields([
+            'c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.image',
+            'p.id',
+        ])
+        .slice(startVal, endVal)
+        .sort({ id: .1 })
+        .getAll()
+        .then(prods => {
+            return prods;
+        });
 }
-else {
-    startVal = 0; 
-    endVal = 10;
-}
 
-database.table('products as p')
-    .join([{
-        table: 'categories as c',
-        on: 'c.id = p.cat_id',
-    }])
-    .withFields([
-        'c.title as category',
-        'p.title as name',
-        'p.price',
-        'p.quantity',
-        'p.image',
-        'p.id',
-    ])
-    .slice(startVal, endVal)
-    .sort({ id: .1 })
-    .getAll()
-    .then(prods => {
-        return prods;
-    });
+module.exports = {
+    getAll,
+}
