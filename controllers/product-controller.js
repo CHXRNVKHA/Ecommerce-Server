6dff21dd9ab82abcd4ef667cff8fcdda261de193
Dmitry Veko <vekodmitriy0@gmail.com>
2020-05-26 12:34:41 +0300

    feat: add product controller

---
+++ controllers/product-controller.js
@@ -0,0 +1,15 @@
+const service = require('');
+class ProductController {
+    constructor(){};
+    getAllProducts = async (req, res) => {
+        try {
+            const result = await service.getAll(req);
+            res.send(result);
+        }
+        catch (e) {
+            res.status(400).send({error:e.message});
+        }
+    };
+}
+
+module.exports = ProductController;
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {database} = require('../config/helpers');
 
-const ProductController = require('');
+const ProductController = require('../controllers/product-controller');
 const productController = new ProductController();
 
 const router = express.Router();
 
-router.get('/', productController.getAll);
+router.get('/', productController.getAllProducts);
 
 module.exports = router;
